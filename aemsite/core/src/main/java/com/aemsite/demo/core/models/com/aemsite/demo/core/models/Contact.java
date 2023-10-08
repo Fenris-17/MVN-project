9aae592d15acc62661aaf12dfb43dc591596a9bb
Fenris-17 <dobhalsaaket@gmail.com>
2023-10-08 18:24:14 +0530

    contactinfo

--- aemsite/core/src/main/java/com/aemsite/demo/core/models/com/aemsite/demo/core/models/Contact.java
+++ aemsite/core/src/main/java/com/aemsite/demo/core/models/com/aemsite/demo/core/models/Contact.java
@@ -7,15 +7,19 @@ import javax.inject.Inject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.api.resource.Resource;
-@Model(adaptables = Resource.class,
-resourceType= Contact.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-@Exporter(name="jackson" , extensions="json")
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name="jackson",selector="test1", extensions="json")
+
+
+
 public class Contact {
-    static final String RESOURCE_TYPE="/apps/aemsite/components/contactinfo";
+    
     @ValueMapValue
     private String contactName;
     @ValueMapValue
     private String contactEmail;
+
     public String getContactName() {
         return contactName;
     }
